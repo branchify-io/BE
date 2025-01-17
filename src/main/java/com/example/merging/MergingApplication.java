db32e34a17cc0544bff40508ed74e5e045e5233b
nam <yhnham@gmail.com>
2025-01-17 12:51:01 +0900

    file upload

---
+++ src/main/java/com/example/merging/MergingApplication.java
@@ -0,0 +1,13 @@
+package com.example.merging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergingApplication.class, args);
+	}
+
+}
