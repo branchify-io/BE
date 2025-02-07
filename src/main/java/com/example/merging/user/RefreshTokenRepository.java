1ebbebdfc249713bae8f5b1c5d4ee6cd764df205
nam <yhnham@gmail.com>
2025-02-07 22:10:31 +0900

    feat: 로그아웃 api 개발

--- src/main/java/com/example/merging/user/RefreshTokenRepository.java
+++ src/main/java/com/example/merging/user/RefreshTokenRepository.java
@@ -1,11 +1,22 @@
 package com.example.merging.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 
-    // 만료된 토큰 삭제
+    // 만료된 토큰 삭제 (스케줄러용)
     void deleteByCreatedAtBefore(LocalDateTime cutoffDate);
+
+    @Modifying
+    @Transactional
+    void deleteByEmail(String email);
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
